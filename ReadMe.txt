387d15ba337fee5bcdc62f873790c6f90161bd5c
atsurinBeatrice <atsurin.ange@icloud.com>
2025-10-10 13:54:16 +0900

    Ak feature1 will be merged to main (#2)
    * 1st commit in dev branch
    
    * Updated by kitagawa
    
    * 1st commit
    
    * join to ak_feature1 branch
    
    * join to ak_feature1 branch
    
    * Modified
    
    ---------
    
    Co-authored-by: atsurinange <atsushi.kitagawa@dxc.com>

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,3 @@
 #Updated by ruser1 on main branch directly.
 ReadMe
+This file is updated in ak_feature1 branch... on 2025/10/10
---
+++ test/20250813_test.r
@@ -0,0 +1,8 @@
+## hatena.R
+## updated by kitagawa
+## updated by ruser1 in ak_feature1 branch
+library(tidyr)
+library(haven)
+a <- sqrt(2)  
+print(a)
+
